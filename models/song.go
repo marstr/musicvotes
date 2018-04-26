db2997849437462643f63eb24b2ffad22d922de3
Martin Strobel <marstr@microsoft.com>
2018-04-25 23:55:49 -0700

    Deleting troublesome song fields.
    I don't take advantage of them, and they're chaffing with my db drvier for some reason. This fixes that.

--- models/song.go
+++ models/song.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"encoding/json"
-	"time"
 
 	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/uuid"
@@ -11,13 +10,11 @@ import (
 )
 
 type Song struct {
-	ID        uuid.UUID `json:"id" db:"id"`
-	Title     string    `json:"title" db:"title"`
-	Artist    string    `json:"artist" db:"artist"`
-	Vote      int       `json:"vote" db:"vote"`
-	Url       string    `json:"url" db:"url"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID     uuid.UUID `json:"id" db:"id"`
+	Title  string    `json:"title" db:"title"`
+	Artist string    `json:"artist" db:"artist"`
+	Vote   int       `json:"vote" db:"vote"`
+	Url    string    `json:"url" db:"url"`
 }
 
 // String is not required by pop and may be deleted
